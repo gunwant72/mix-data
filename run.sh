f476f940259e2afe3a29754ffd328c57161cd8cc
gunwant72 <gunwant@unstudio.ai>
2024-04-19 14:53:42 +0530

    new data

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Array of file keys
-file_keys=(chill_ice_v1)
+file_keys=(glass_platform lily_flower white_platform_v1 white_platform_v1 stone_platform wood_snow wooden_shelf_final pure_palette_final Wooden_Podium wooden_platform water_wood marble_bird_eye)
 # Download files from S3
 for key in "${file_keys[@]}"; do
-    aws s3 cp "s3://unstudio-product-photoshoots/dataset/Lora-Categories/$key" "./data/$key" --recursive
+    aws s3 cp "s3://unstudio-product-photoshoots/dataset/Lora-Categories/$key" "./data/min" --recursive
 done
 
 # Run main.py (replace 'main.py' with the actual name of your Python script)
-pip install -r requirements.txt
+# pip install -r requirements.txt
 python caption.py
 
